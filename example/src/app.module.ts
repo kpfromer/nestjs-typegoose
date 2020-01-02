48719bd225676663c71977b1ac72a140c0130c29
Kyle Pfromer <kpfromer2@gmail.com>
2020-01-02 11:31:24 -0700

    Add example project

---
+++ example/src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { CatsModule } from './cat/cats.module';
+
+@Module({
+  imports: [
+    TypegooseModule.forRoot('mongodb://localhost:27017/nest', {
+      useNewUrlParser: true,
+    }),
+    CatsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ example/src/cat/cats.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from 'nestjs-typegoose';
+import { Cat } from './cat.model';
+import { ReturnModelType } from '@typegoose/typegoose';
+
+@Injectable()
+export class CatsService {
+  constructor(
+    @InjectModel(Cat) private readonly catModel: ReturnModelType<typeof Cat>,
+  ) {}
+
+  async create(createCatDto: { name: string }): Promise<Cat> {
+    const createdCat = new this.catModel(createCatDto);
+    return await createdCat.save();
+  }
+
+  async findAll(): Promise<Cat[] | null> {
+    return await this.catModel.find().exec();
+  }
+}
