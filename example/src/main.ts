ea38236ce8b124417dcb4f2d76122836e8d04e5c
Kyle Pfromer <kpfromer2@gmail.com>
2020-01-06 15:12:20 -0700

    Refactor bootstrap to IIFE

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,8 +1,7 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
-async function bootstrap() {
+(async function() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-}
-bootstrap();
+})();
