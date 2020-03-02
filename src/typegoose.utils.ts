5c12c58cbef27c83b7a1ffafffa0a6b50e5854d8
Kyle Pfromer <kpfromer2@gmail.com>
2020-03-02 13:01:31 -0700

    Add utils tsdoc

--- src/typegoose.utils.ts
+++ src/typegoose.utils.ts
@@ -1,9 +1,21 @@
 import { DEFAULT_DB_CONNECTION_NAME } from './typegoose.constants';
 
+/**
+ * Returns the provider token name.
+ * @param model The model name
+ * @returns The token name
+ * @internal
+ */
 export function getModelToken(model: string) {
   return `${model}Model`;
 }
 
+/**
+ * Returns the provider connection token name.
+ * @param name the name of the connection
+ * @returns the connection provider name
+ * @internal
+ */
 export function getConnectionToken(name?: string) {
   if (typeof name === 'string' && name !== DEFAULT_DB_CONNECTION_NAME) {
     return `${name}Connection`;
